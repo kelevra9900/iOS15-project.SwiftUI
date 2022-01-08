3618d7454c810bbdfb8c5a95fc28dcab070446c1
Rogelio Torres <torresroger445@gmail.com>
2022-01-07 19:58:11 -0600

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Rogelio Torres on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
