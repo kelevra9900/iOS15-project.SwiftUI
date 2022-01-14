ce9421e2b56c62303b69c66ea3c8b8b2c7ddac66
Rogelio Torres <torresroger445@gmail.com>
2022-01-13 19:50:53 -0600

    Adding Navigation TopBar

--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -9,50 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 8.0) {
-            Spacer()
-            Image("Logo 2")
-                .resizable(resizingMode: .stretch)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 26.0, height: 26.0)
-                .cornerRadius(10.0)
-                .padding(9)
-                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
-                .strokeStyle(cornerRadius: 16)
-            Text("SwiftUI for iOS 15!")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .foregroundStyle(.secondary)
-            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
-                .font(.footnote)
-                .multilineTextAlignment(.leading)
-                .lineLimit(2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundStyle(.secondary)
+        ScrollView {
+            FeaturedItem()
         }
-        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
-        .padding(.vertical, 20.0)
-        .frame(height: 350.0)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
-        //.cornerRadius(30.0)
-        //.mask(RoundedRectangle(cornerRadius: 30, style: .continuo.strokeStyle()us))
-        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-        .strokeStyle()
-        .padding(.horizontal, 20.0)
-        .background(
-            Image("Blob 1")
-                .offset(x: 250, y: -100)
-        )
         .overlay(
-            Image("Illustration 5")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 230)
-                .offset(x: 32, y: -80)
+            NavigationBar(title: "Featured")
         )
     }
 }
