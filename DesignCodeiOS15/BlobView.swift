bd13909ed171650bacab3ad19ab469cd7625a384
Rogelio Torres <torresroger445@gmail.com>
2022-01-13 19:07:41 -0600

    BlobView adding to contentView

---
+++ DesignCodeiOS15/BlobView.swift
@@ -0,0 +1,20 @@
+//
+//  BlobView.swift
+//  DesignCodeiOS15
+//
+//  Created by Rogelio Torres on 12/01/22.
+//
+
+import SwiftUI
+
+struct BlobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlobView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlobView()
+    }
+}
